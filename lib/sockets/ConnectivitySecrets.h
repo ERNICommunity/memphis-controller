bba21366f25e7a410b898796b53ca0a8b362320f
Dieter Niklaus <dieter.niklaus@gmx.net>
2018-02-12 19:21:46 +0100

    cleanup ConnectivitySecrets.h (WiFi credentials to be entered interactively on Cmd CLI).

--- lib/sockets/ConnectivitySecrets.h
+++ lib/sockets/ConnectivitySecrets.h
@@ -8,9 +8,6 @@
 #ifndef LIB_SOCKETS_CONNECTIVITYSECRETS_H_
 #define LIB_SOCKETS_CONNECTIVITYSECRETS_H_
 
-//const char* WIFI_SSID = "TECHNOARK";
-//const char* WIFI_PWD  = "";
-
 const unsigned long int cMyChannelNumber = 116546;
 const char* cMyWriteAPIKey = "OBXOZH2F394WZSQM";
 
